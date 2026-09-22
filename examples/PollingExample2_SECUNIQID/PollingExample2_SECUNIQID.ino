6accd0ffbdd6abfe54ed2789dd77173b93c3bc41
PaulZC <pc235603@gmail.com>
2026-09-22 12:21:39 +0100

    Create PollingExample2_SECUNIQID.ino

---
+++ examples/PollingExample2_SECUNIQID/PollingExample2_SECUNIQID.ino
@@ -0,0 +1,58 @@
+/*
+  Polling the u-blox module's SEC-UNIQID
+  By: Paul Clark
+  SparkFun Electronics
+  Date: September 2026
+  License: MIT. Please see LICENSE.md for more information.
+
+  u-blox GNSS modules contain a unique 6-byte ID which can be read using
+  the UBX-SEC-UNIQID message. In this example, we poll the SEC-UNIQID message.
+  The helper method getUniqueChipIdStr() converts the 6 bytes to hexadecimal String.
+  
+  Feel like supporting open source hardware?
+  Buy a board from SparkFun!
+  https://www.sparkfun.com/sparkfun-allband-gnss-rtk-breakout-zed-x20p-qwiic.html
+  https://www.sparkfun.com/sparkfun-gps-rtk2-board-zed-f9p-qwiic-gps-15136.html
+  https://www.sparkfun.com/sparkfun-gps-rtk-sma-breakout-zed-f9p-qwiic.html
+  https://www.sparkfun.com/sparkfun-gnss-receiver-breakout-max-m10s-qwiic.html
+  https://www.sparkfun.com/sparkfun-gps-rtk-dead-reckoning-breakout-zed-f9r-qwiic-gps-22693.html
+
+  Hardware Connections:
+  Plug a Qwiic cable into the GNSS and your microcontroller board
+  Open the serial monitor at 115200 baud to see the output
+*/
+
+#include <Wire.h> //Needed for I2C to GNSS
+
+#include <SparkFun_u-blox_GNSS_v4.h> //http://librarymanager/All#SparkFun_u-blox_GNSS_v4
+
+SFE_UBLOX_GNSS myGNSS; // SFE_UBLOX_GNSS uses I2C
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000); 
+  Serial.println("SparkFun u-blox Example");
+
+  Wire.begin(); // Start I2C
+
+  //myGNSS.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
+
+  while (myGNSS.begin() == false) //Connect to the u-blox module using Wire port
+  {
+    Serial.println("u-blox GNSS not detected at default I2C address. Retrying...");
+    delay (1000);
+  }
+
+  if (myGNSS.getSECUNIQID()) // Poll the SEC-UNIQID message
+  {
+    String uniqueId = myGNSS.getUniqueChipIdStr();
+    Serial.print("Unique chip ID: 0x");
+    Serial.println(uniqueId);
+  }
+}
+
+void loop()
+{
+  // Nothing to do here...
+}
